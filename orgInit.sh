e681bd7f0aa1af62b57d71973b5d5b9486b6889d
mehboob <77236545+mehb1@users.noreply.github.com>
2025-03-24 18:15:26 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,14 +1,1 @@
-# # sfdx force:apex:log:list --target-org hub 
-# # sfdx config:list --target-org hub 
-# # sfdx force:apex:log:get --target-org hub 
-# # sfdx config:get --target-org hub
-# # sfdx force:config:list --configfile /app/.sf/config.json 
-# sfdx force:mdapi:retrieve -o hub
-# sfdx org display --target-org hub --verbose
-# sfdx auth:list
-# sfdx force:org:list
-# sfdx force:user:list --target-org hub
-# sfdx force:user:list --target-org deploybot@dxhub.org
-# sfdx login:functions:jwt --clientid 3MVG9CEn_O3jvv0zQGZ3RC8AeiKOvTHGF1AyKTPEhceKFjU7hrwn1JO3h.JnCWqOUo.ZQ9iGALbY1Fy_taX3y --username deploybot@dxhub.org --keyfile /app/tmp/server.key
-# sfdx shane:data:file:upload -f /etc/passwd -n zeb0x01test -u deploybot@dxhub.org
 sf force:org:open -o deploybot@dxhub.org -r
